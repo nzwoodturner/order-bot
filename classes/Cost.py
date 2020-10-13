3fa4c05c9ca5e52621842b66b1f07edd3d6f684e
nzwoodturner <48368010+nzwoodturner@users.noreply.github.com>
2020-10-13 14:50:03 +1300

    cost skills implemented

--- classes/Cost.py
+++ classes/Cost.py
@@ -12,7 +12,7 @@ class Cost():
             "Cobaltite":20,"Cryolite":20,"Kolbeckite":20,"Gold Nuggets":20,
             "Rhodonite":20,"Columbite":20,"Illmenite":20,"Vanadinite":20}    
 
-    itemTier: ["Basic",
+    itemTier= ["Basic",
            "Uncommon",
            "Advanced",
            "Rare",
@@ -25,55 +25,55 @@ class Cost():
     def find(self,element):
         for i in range (len(self.recipes)):
             if (self.recipes[i]["name"]==element):
-                print('1')
                 return self.recipes[i]
         return False
 
     def findSkill(self,recipe):
-        craftingSkills= {"input":1,"output":1,"time":1}              
-        for i in skills:
-            if (skills[i].has_key("target")==False):
-                for j in skills[i]["data"]:
-                    if (skills[i]["data"][j]==recipe["name"]):
-                        value=skills[i]["targets"][j]["amount"]*skills[i]["values"][j]
-                        craftingSkills[skills[i]["targets"][j]["type"]]=value
+        craftingSkills= {"Input":0,"Output":0,"Time":0}              
+        for i in range (len(self.skills)):
+            if (("target" in self.skills[i])==False):
+                for j in range (len(self.skills[i]["data"])):
+                    if (self.skills[i]["data"][j]==recipe["name"]):
+                        value+=self.skills[i]["targets"][j]["amount"]*self.skills[i]["values"][j]
+                        craftingSkills[self.skills[i]["targets"][j]["type"]]=value
             else:
-                if(skills[i]["target"]==recipe["type"]):
-                    for j in skills[i]["data"]:
-                        if (skills[i]["data"][j]==recipe["name"]):
-                            value=skills[i]["targets"][j]["amount"]*skills[i]["values"][j]
-                            craftingSkills[skills[i]["targets"][j]["type"]]=value
-                        elif (skills[i]["data"][j]=="Efficiency"):
-                            value=skills[i]["targets"][j]["amount"]*skills[i]["values"][j]
-                            craftingSkills[skills[i]["targets"][j]["type"]]=value
-                        elif (tier(skills[i]["data"][j])==recipe["tier"]):
-                            if (recipe["type"]!="Intermediary Part"):
-                                value=skills[i]["targets"][j]["amount"]*skills[i]["values"][j]
-                                craftingSkills[skills[i]["targets"][j]["type"]]=value
+                if(self.skills[i]["target"]==recipe["type"]):
+                    for j in range  (len(self.skills[i]["data"])):
+                        if (self.skills[i]["data"][j]==recipe["name"]):
+                            value=self.skills[i]["targets"][j]["amount"]*self.skills[i]["values"][j]
+                            craftingSkills[self.skills[i]["targets"][j]["type"]]+=value
+                        elif (self.skills[i]["data"][j]=="Efficiency"):
+                            value=self.skills[i]["targets"][j]["amount"]*self.skills[i]["values"][j]
+                            craftingSkills[self.skills[i]["targets"][j]["type"]]+=value
+                        elif (self.tier(self.skills[i]["data"][j])==recipe["tier"]):
+                            if ((recipe["type"]=="Intermediary Part") and (self.skills[i]["targets"][j]["type"]=="Output")):
+                                value=0.1*self.skills[i]["values"][j]
+                                craftingSkills[self.skills[i]["targets"][j]["type"]]+=value
                             else:
-                                value=0.1*skills[i]["values"][j]
-                                craftingSkills[skills[i]["targets"][j]["type"]]=value
-
+                                value=self.skills[i]["targets"][j]["amount"]*self.skills[i]["values"][j]
+                                craftingSkills[self.skills[i]["targets"][j]["type"]]+=value
+        return craftingSkills
 
 
     def tier(self,name):
-        for i in itemTier:
-            if (itemTier[i]==name):
-                return i
+        for i in range (len(self.itemTier)):
+            if (self.itemTier[i]==name):
+                return i+1
         return False
 
     def get_cost(self,element,number):
 
         recipe=self.find(element)
+        skill=self.findSkill(recipe)
         cost=0
         if recipe != False:
             if recipe["type"]=="Ore":
-                return self.price_list[element]*(number/recipe["outputQuantity"])
+                return self.price_list[element]*number
             elif recipe["type"]!="Catalyst":
                 for key in recipe["input"]:
-                    outputQuantity=recipe["outputQuantity"]
-                    numberNeeded= recipe["input"][key]*(number/outputQuantity)
-                    cost=cost+self.get_cost(key,numberNeeded)
+                    outputQuantity=recipe["outputQuantity"]*(1+skill["Output"])
+                    numberNeeded= (recipe["input"][key]*(1-skill["Input"]))*(number/outputQuantity)
+                    cost+=self.get_cost(key,numberNeeded)
                 return cost
             else:
                 return 0
